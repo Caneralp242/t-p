96a9672e1c8ec964a8aa3421b339b265ddff0585
Caneralp242 <76888053+Caneralp242@users.noreply.github.com>
2022-01-11 00:21:56 +0300

    Delete 4-P.I.py

--- 4-P.I.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-
-df=pd.read_csv("/Users/caneralp/Downloads/country_vaccination_stats.csv")
-
-df["daily_vaccinations"]=df["daily_vaccinations"].fillna(df.groupby("country")["daily_vaccinations"].transform("min"))
-
-df
-
